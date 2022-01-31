5d8caebc0cde64a59001328a7658fc923b4fd9ad
Timofey Titovets <nefelim4ag@gmail.com>
2022-01-31 04:18:02 +0300

    Revert "Use safe default hybrid thresholds"
    This reverts commit 732a4de9cb14f8f76461180cfe591216da2b5f05.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3063,9 +3063,9 @@
     #define HYBRID_THRESHOLD
   #endif
 
-  #define X_HYBRID_THRESHOLD     0  // [mm/s]
+  #define X_HYBRID_THRESHOLD     200  // [mm/s]
   #define X2_HYBRID_THRESHOLD    100
-  #define Y_HYBRID_THRESHOLD     0
+  #define Y_HYBRID_THRESHOLD     200
   #define Y2_HYBRID_THRESHOLD    100
   #define Z_HYBRID_THRESHOLD       3
   #define Z2_HYBRID_THRESHOLD      3
@@ -3074,7 +3074,7 @@
   #define I_HYBRID_THRESHOLD       3
   #define J_HYBRID_THRESHOLD       3
   #define K_HYBRID_THRESHOLD       3
-  #define E0_HYBRID_THRESHOLD     5
+  #define E0_HYBRID_THRESHOLD     30
   #define E1_HYBRID_THRESHOLD     30
   #define E2_HYBRID_THRESHOLD     30
   #define E3_HYBRID_THRESHOLD     30
