2a7adb3f6b085d59cfb6d43b9f3cc3673b0e4873
AndyBig <andybig@gmail.com>
2022-07-26 04:15:45 +0300

    Export settings to file

--- Marlin/src/module/filesettings_strings.h
+++ Marlin/src/module/filesettings_strings.h
@@ -16,39 +16,57 @@
 #pragma once
 
 
-extern const char* FSS_MOTION_ACCEL_MAX_X;
-extern const char* FSSC_MOTION_ACCEL_MAX_X;
-extern const char* FSS_MOTION_ACCEL_MAX_Y;
-extern const char* FSSC_MOTION_ACCEL_MAX_Y;
-extern const char* FSS_MOTION_ACCEL_MAX_Z;
-extern const char* FSSC_MOTION_ACCEL_MAX_Z;
-extern const char* FSS_MOTION_ACCEL_MAX_E;
-extern const char* FSSC_MOTION_ACCEL_MAX_E;
-extern const char* FSS_MOTION_PRINT_ACCEL;
-extern const char* FSSC_MOTION_PRINT_ACCEL;
-extern const char* FSS_MOTION_RETRACT_ACCEL;
-extern const char* FSSC_MOTION_RETRACT_ACCEL;
-extern const char* FSS_MOTION_TRAVEL_ACCEL;
-extern const char* FSSC_MOTION_TRAVEL_ACCEL;
-extern const char* FSS_MOTION_MIN_SEGMENT_TIME;
-extern const char* FSSC_MOTION_MIN_SEGMENT_TIME;
-extern const char* FSS_MOTION_STEPS_PER_MM_X;
-extern const char* FSSC_MOTION_STEPS_PER_MM_X;
-extern const char* FSS_MOTION_STEPS_PER_MM_Y;
-extern const char* FSSC_MOTION_STEPS_PER_MM_Y;
-extern const char* FSS_MOTION_STEPS_PER_MM_Z;
-extern const char* FSSC_MOTION_STEPS_PER_MM_Z;
-extern const char* FSS_MOTION_STEPS_PER_MM_E;
-extern const char* FSSC_MOTION_STEPS_PER_MM_E;
-extern const char* FSS_MOTION_MAX_SPEED_X;
-extern const char* FSSC_MOTION_MAX_SPEED_X;
-extern const char* FSS_MOTION_MAX_SPEED_Y;
-extern const char* FSSC_MOTION_MAX_SPEED_Y;
-extern const char* FSS_MOTION_MAX_SPEED_Z;
-extern const char* FSSC_MOTION_MAX_SPEED_Z;
-extern const char* FSS_MOTION_MAX_SPEED_E;
-extern const char* FSSC_MOTION_MAX_SPEED_E;
-extern const char* FSS_MOTION_MIN_PRINT_SPEED;
-extern const char* FSSC_MOTION_MIN_PRINT_SPEED;
-extern const char* FSS_MOTION_MIN_TRAVEL_SPEED;
-extern const char* FSSC_MOTION_MIN_TRAVEL_SPEED;
+const char FSS_MOTION_ACCEL_MAX_X[] = "Motion_Accelerate_Max_X";
+const char FSSC_MOTION_ACCEL_MAX_X[] = " # mm/s2, M201 X";
+
+const char FSS_MOTION_ACCEL_MAX_Y[] = "Motion_Accelerate_Max_Y";
+const char FSSC_MOTION_ACCEL_MAX_Y[] = " # mm/s2, M201 Y";
+
+const char FSS_MOTION_ACCEL_MAX_Z[] = "Motion_Accelerate_Max_Z";
+const char FSSC_MOTION_ACCEL_MAX_Z[] = " # mm/s2, M201 Z";
+
+const char FSS_MOTION_ACCEL_MAX_E[] = "Motion_Accelerate_Max_E";
+const char FSSC_MOTION_ACCEL_MAX_E[] = " # mm/s2, M201 E";
+
+const char FSS_MOTION_PRINT_ACCEL[] = "Motion_Print_Accelerate";
+const char FSSC_MOTION_PRINT_ACCEL[] = " # mm/s2, M204 S";
+
+const char FSS_MOTION_RETRACT_ACCEL[] = "Motion_Retract_Accelerate";
+const char FSSC_MOTION_RETRACT_ACCEL[] = " # mm/s2, M204 R";
+
+const char FSS_MOTION_TRAVEL_ACCEL[] = "Motion_Travel_Accelerate";
+const char FSSC_MOTION_TRAVEL_ACCEL[] = " # mm/s2, M204 T";
+
+const char FSS_MOTION_MIN_SEGMENT_TIME[] = "Motion_Min_Segment_Time";
+const char FSSC_MOTION_MIN_SEGMENT_TIME[] = " # µs, M205 B";
+
+const char FSS_MOTION_STEPS_PER_MM_X[] = "Motion_Steps_Per_Mm_X";
+const char FSSC_MOTION_STEPS_PER_MM_X[] = " # steps, M92 X";
+
+const char FSS_MOTION_STEPS_PER_MM_Y[] = "Motion_Steps_Per_Mm_Y";
+const char FSSC_MOTION_STEPS_PER_MM_Y[] = " # steps, M92 Y";
+
+const char FSS_MOTION_STEPS_PER_MM_Z[] = "Motion_Steps_Per_Mm_Z";
+const char FSSC_MOTION_STEPS_PER_MM_Z[] = " # steps, M92 Z";
+
+const char FSS_MOTION_STEPS_PER_MM_E[] = "Motion_Steps_Per_Mm_E";
+const char FSSC_MOTION_STEPS_PER_MM_E[] = " # steps, M92 E";
+
+const char FSS_MOTION_MAX_SPEED_X[] = "Motion_Max_Speed_X";
+const char FSSC_MOTION_MAX_SPEED_X[] = " # mm/s, M203 X";
+
+const char FSS_MOTION_MAX_SPEED_Y[] = "Motion_Max_Speed_Y";
+const char FSSC_MOTION_MAX_SPEED_Y[] = " # mm/s, M203 Y";
+
+const char FSS_MOTION_MAX_SPEED_Z[] = "Motion_Max_Speed_Z";
+const char FSSC_MOTION_MAX_SPEED_Z[] = " # mm/s, M203 Z";
+
+const char FSS_MOTION_MAX_SPEED_E[] = "Motion_Max_Speed_E";
+const char FSSC_MOTION_MAX_SPEED_E[] = " # mm/s, M203 E";
+
+const char FSS_MOTION_MIN_PRINT_SPEED[] = "Motion_Min_Print_Speed";
+const char FSSC_MOTION_MIN_PRINT_SPEED[] = " # mm/s, M205 S";
+
+const char FSS_MOTION_MIN_TRAVEL_SPEED[] = "Motion_Min_Travel_Speed";
+const char FSSC_MOTION_MIN_TRAVEL_SPEED[] = " # mm/s, M205 T";
+
