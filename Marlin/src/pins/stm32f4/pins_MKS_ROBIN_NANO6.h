010a155be155baa9076ef5ae5494255859f22167
Timofey Titovets <nefelim4ag@gmail.com>
2022-11-17 17:27:24 +0400

    reborn2: try random magic

--- Marlin/src/pins/stm32f4/pins_MKS_ROBIN_NANO6.h
+++ Marlin/src/pins/stm32f4/pins_MKS_ROBIN_NANO6.h
@@ -24,8 +24,6 @@
 #define ALLOW_STM32DUINO
 #include "env_validate.h"
 
-#define SDCARD_CONNECTION ONBOARD
-
 #include "pins_MKS_ROBIN_NANO_V3_common.h"
 
 #if HOTENDS > 2 || E_STEPPERS > 2
@@ -36,43 +34,41 @@
 
 #define BOARD_INFO_NAME "MKS Robin Nano6"
 
+// #ifndef X_CS_PIN
+//   #define X_CS_PIN                          PD5
+// #endif
 
+// #ifndef Y_CS_PIN
+//   #define Y_CS_PIN                          PD7
+// #endif
 
-#ifndef X_CS_PIN
-  #define X_CS_PIN                          PD5
-#endif
+// #ifndef Z_CS_PIN
+//   #define Z_CS_PIN                          PD4
+// #endif
 
-#ifndef Y_CS_PIN
-  #define Y_CS_PIN                          PD7
-#endif
+// #ifndef E0_CS_PIN
+//   #define E0_CS_PIN                         PD9
+// #endif
 
-#ifndef Z_CS_PIN
-  #define Z_CS_PIN                          PD4
-#endif
+// #ifndef E1_CS_PIN
+//   #define E1_CS_PIN                         PD8
+// #endif
 
-#ifndef E0_CS_PIN
-  #define E0_CS_PIN                         PD9
-#endif
+// #ifndef X_ENABLE_PIN
+//     #define X_ENABLE_PIN                    PE4
+// #endif
 
-#ifndef E1_CS_PIN
-  #define E1_CS_PIN                         PD8
-#endif
+// #ifndef Y_ENABLE_PIN
+//     #define Y_ENABLE_PIN                    PE1
+// #endif
 
-#ifndef X_ENABLE_PIN
-    #define X_ENABLE_PIN                    PE4
-#endif
+// #ifndef E0_DIAG_PIN
+//     #define E1_DIAG_PIN                     -1
+// #endif
 
-#ifndef Y_ENABLE_PIN
-    #define Y_ENABLE_PIN                    PE1
-#endif
-
-#ifndef E0_DIAG_PIN
-    #define E1_DIAG_PIN                     -1
-#endif
-
-#ifndef E1_DIAG_PIN
-    #define E1_DIAG_PIN                     -1
-#endif
+// #ifndef E1_DIAG_PIN
+//     #define E1_DIAG_PIN                     -1
+// #endif
 
 // #define E2_ENABLE_PIN                       PA3
 // #define E2_STEP_PIN                         PE1
@@ -92,30 +88,24 @@
 #undef FAN_PIN
 #define FAN_PIN PB1
 
-// #undef HEATER_0_PIN
-// #undef HEATER_1_PIN
-// Swap heaters pin
-// #define HEATER_0_PIN                        PB0   // HEATER1
-// #define HEATER_1_PIN                        PE5   // HEATER2
-
 //
 // Software SPI pins for TMC2130 stepper drivers
 // This board only supports SW SPI for stepper drivers
 //
-#if HAS_TMC_SPI
-  #define TMC_USE_SW_SPI
-#endif
-#if ENABLED(TMC_USE_SW_SPI)
-  #if !defined(TMC_SW_MOSI) || TMC_SW_MOSI == -1
-    #define TMC_SW_MOSI                     PD14
-  #endif
-  #if !defined(TMC_SW_MISO) || TMC_SW_MISO == -1
-    #define TMC_SW_MISO                     PD1
-  #endif
-  #if !defined(TMC_SW_SCK) || TMC_SW_SCK == -1
-    #define TMC_SW_SCK                      PD0
-  #endif
-#endif
+// #if HAS_TMC_SPI
+//   #define TMC_USE_SW_SPI
+// #endif
+// #if ENABLED(TMC_USE_SW_SPI)
+//   #if !defined(TMC_SW_MOSI) || TMC_SW_MOSI == -1
+//     #define TMC_SW_MOSI                     PD14
+//   #endif
+//   #if !defined(TMC_SW_MISO) || TMC_SW_MISO == -1
+//     #define TMC_SW_MISO                     PD1
+//   #endif
+//   #if !defined(TMC_SW_SCK) || TMC_SW_SCK == -1
+//     #define TMC_SW_SCK                      PD0
+//   #endif
+// #endif
 
 #define Z_MIN_PROBE_PIN                    PE6
 
